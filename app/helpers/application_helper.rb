160ac127779cbc9a23f850007caa80e51b5a5156
Marlon Andrade <marlonmandrade@gmail.com>
2014-07-05 16:28:30 -0300

    validation to links and adding http if theres not

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def url_with_protocol(url)
+    unless url =~ /\Ahttp.*/
+      url = "http://#{url}"
+    end
+    url
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,11 @@
 class Link < ActiveRecord::Base
+
   belongs_to :user
+
+  validates :name, :url, presence: true
+  validates :url, format: {
+    with: /\A((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/,
+    message: 'invalid link format'
+  }
+
 end
