46c10e261aee89df4f4cb9229b86c74456ea41ed
Marlon Andrade <marlonmandrade@gmail.com>
2014-07-05 13:41:37 -0300

    adicionando links scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth auth
+    if user = User.find_by_email(auth.info.email)
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user
+    else
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.username = auth.info.name
+        user.email = auth.info.email
+        user.avatar = auth.info.image
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705161630) do
+ActiveRecord::Schema.define(version: 20140705163800) do
+
+  create_table "links", force: true do |t|
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
