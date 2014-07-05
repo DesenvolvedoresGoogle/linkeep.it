ea57fe1faa1ae2598d4c9401d76733838b17d392
Marlon Andrade <marlonmandrade@gmail.com>
2014-07-05 20:38:02 -0300

    removing home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
-
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   resources :links do
@@ -11,9 +9,7 @@ Rails.application.routes.draw do
     end
   end
   
-  get 'home', to: 'index'
-
-  root 'home#index'
+  root 'links#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
